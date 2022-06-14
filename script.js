a134a565d5c4a64a184f21f5928630fd2637c96a
kevineppi <kevineppensteiner05@gmail.com>
2022-06-14 10:39:09 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello!')
+function postData(path){
+
+ 
+ document.getElementById("form").action = path;
+ document.getElementById("form").submit();
+}
+
+postData("http://localhost:8080/F_G_Uebung_Service/test2")
